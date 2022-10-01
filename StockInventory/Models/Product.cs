d3a255532ba0b372ed3cc2bbe02a2c9e6bc2e77c
timuadib20 <timajadib1@gmail.com>
2022-10-01 22:33:38 +0300

    added validation to each class

--- StockInventory/Models/Product.cs
+++ StockInventory/Models/Product.cs
@@ -6,9 +6,15 @@ namespace StockInventory.Models
     {
         [Key]
         public long ProductId { get; set; }
+        [Required]
         public string ProductName { get; set; }
+        [StringLength(50,ErrorMessage ="Limited charaters")]
         public string Description { get; set; }
+       
+        [RegularExpression("^[0-9]*$", ErrorMessage = " Must be numeric")]
         public int Quantity { get; set; }
+        
+        [RegularExpression("^[0-9]*$", ErrorMessage = " Must be numeric")]
         public double Price { get; set; }
         public virtual User User { get; set; }
     }
